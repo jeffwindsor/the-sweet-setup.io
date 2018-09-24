d41126a075e926fa26b95eb851cd8dd5ff55ef32
Jeff Windsor <jwindsor@cj.com>
2018-09-24 13:30:19 -0700

    master : added lodash join

--- src/resolvers/queries/fragment.js
+++ src/resolvers/queries/fragment.js
@@ -3,26 +3,34 @@ import curlResolver from './curl'
 import bashFunctionResolver from './bashfunction'
 import fishFunctionResolver from './fishfunction'
 import sectionResolver from './section'
+var _join = require('lodash/join');
 
 // ? make output language specific
 //Parses fragment object by type into sh sting
 export default function(o){
   switch (o.type) {
-   case 'ArchPackage': return `sudo pacman -S --noconfirm ${o.name}`;
-   case 'YayPackage': return `yay -S --noconfirm ${o.name}`;
-   case 'BrewPackage': return `brew install ${o.name}`;
-   case 'CaskPackage': return `brew cask install ${o.name}`;
-   case 'NpmPackage': return `npm install ${o.name}`;
-   case 'VsCodeExtension': return `code --install-extension ${o.name}`;
-   case 'HaskellStackInstall': return `stack install ${o.name}`;
+   case 'ArchPackage': return spaced('sudo pacman','-S --noconfirm', o.name);
+   case 'YayPackage': return spaced('yay', '-S --noconfirm', o.name);
+   case 'BrewPackage': return spaced('brew install', o.name);
+   case 'CaskPackage': return spaced('brew cask install', o.name);
+   case 'NpmPackage': return spaced('npm install', o.name);
+   case 'VsCodeExtension': return spaced('code', '--install-extension', o.name);
+   case 'HaskellStackInstall': return spaced('stack install', o.name);
    case 'BashFunction': return bashFunctionResolver(o);
    case 'FishFunction': return fishFunctionResolver(o);
-   case 'GitGlobal': return `git config --global ${o.name} "${o.value}"`;
+   case 'GitGlobal': return spaced('git config', '--global', o.name, ' ', o.value);
    case 'GitClone': return gitCloneResolver(o);
    case 'Curl': return curlResolver(o);
    case 'Section': return sectionResolver(o);
    default: return "";
  }
+}
+
+function spaced(...fragments){
+  return _join(fragments, ' ');
+}
+function equaled(...fragments){
+  return _join(fragments, ' ');
 }
 
 
