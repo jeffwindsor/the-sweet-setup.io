a6fb6fc21265fffc2dbf23e9663506f7718aa4d6
Jeff Windsor <jeff.windsor@gmail.com>
2018-10-09 21:51:27 -0700

    code climate mitigation

--- js/scripting.js
+++ js/scripting.js
@@ -2,7 +2,7 @@
 	SCRIPT
 ***************************************************/
 function script(os, language, requests) {
-  let header = [{ type: 'header', value:'#!/bin/sh' }];
+  let header = [{ type: 'header', value: '#!/bin/sh' }];
   let list = header.concat(requests);
   let tokens = _.flatMap(list, request => tokenize(request));
   let outputs = tokens.map(token => generate(os, language, token));
@@ -14,53 +14,36 @@ function script(os, language, requests) {
 ***************************************************/
 function tokenize(request) {
   switch (request.type.toLowerCase()) {
-
-    case 'header':
-      // ? does not consider language or OS
-      return { type:'comment', comment:'!/bin/sh'};
-
-    case 'fish':
-      return { type: 'file', content: buildFishFunction(request), target: request.target};
-
-    case 'bash':
-      return { type: 'file', content: buildBashFunction(request), target: request.target};
-
-    case 'vscode-package':
-      return _.map(request.extensions, i => {
-        return {type: 'code', extension_name: i.extension_name};
-      });
-
+    case 'header': return { type: 'comment', comment: '!/bin/sh' };
+    case 'fish': return { type: 'file', content: buildFishFunction(request), target: request.target };
+    case 'bash': return { type: 'file', content: buildBashFunction(request), target: request.target };
+    case 'vscode-package': return _.map(request.extensions, i => { 
+      return { type: 'code', extension_name: i.extension_name }; });
     case 'fish-package':
       return _.map(request.functions, i => {
-        let target = (request.target == null)
-          ? {operator:'redirect', path:`~/.config/fish/functions/${i.function_name}.fish`}
-          : request.target;
-        return { type: 'file', content: buildFishFunction(i), target: target};
+        return { type: 'file', content: buildFishFunction(i), target: buidlFishTarget(request.target) };
       });
-
-    case 'bash-package':
-      return _.map(request.functions, i => {
-        return { type: 'file', content: buildBashFunction(i), target: request.target};
-      });
-
-    case 'gitconfig-package':
-      return _.map(request.globals, i => {
-        return { type: 'gitconfig', name: i.name, value: i.value};
-      });
-
-    default:
-      return request;
+    case 'bash-package': return _.map(request.functions, i => { 
+      return { type: 'file', content: buildBashFunction(i), target: request.target }; });
+    case 'gitconfig-package': return _.map(request.globals, i => { 
+      return { type: 'gitconfig', name: i.name, value: i.value }; });
+    default: return request;
   }
 }
 
+function buidlFishTarget(target){
+  return (target == null)
+    ? { operator: 'redirect', path: `~/.config/fish/functions/${i.function_name}.fish` }
+    : target;
+}
 function buildBashFunction(request) {
   return `function ${request.function_name}(){\n  ${request.function_body}\n}`;
 }
 
 function buildFishFunction(request) {
   var body = request.function_body
-                    .replace(/\{@\}/gim, 'argv')
-                    .replace(/\{(\d+)\}/gim, 'argv[$1]');
+    .replace(/\{@\}/gim, 'argv')
+    .replace(/\{(\d+)\}/gim, 'argv[$1]');
   return `function ${request.function_name}\n  ${body}\nend`;
 }
 
